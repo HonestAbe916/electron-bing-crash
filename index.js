fa610257dcb152713c7a15e6d227cc1209097db6
Abe Schor <schor.abe@gmail.com>
2019-05-28 13:36:43 -0400

    inital commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const {ipcRenderer, remote} = require('electron')
+const form = document.getElementById('search-form')
+
+remote.session.defaultSession.clearStorageData(
+	{storages : ['appcache', 'cookies', 'filesystem', 'indexdb', 'localstorage', 'shadercache', 'websql', 'serviceworkers', 'cachestorage']},
+)
+
+onload = () => {
+	const webview = document.querySelector('webview')
+	webview.addEventListener('dom-ready', () => {
+		form.addEventListener('submit', function (e) {
+			e.preventDefault()
+			webview.loadURL(document.getElementById('search').value)
+		})
+	})
+
+	webview.addEventListener('did-navigate', (e) => {
+		document.getElementById('search').value = e.url
+	})
+}
+
+function goBack() {
+	const webview = document.querySelector('webview')
+	if (webview.canGoBack())
+		webview.goBack()
+}
+
+function goForward() {
+	const webview = document.querySelector('webview')
+	if (webview.canGoForward())
+		webview.goForward()
+}
